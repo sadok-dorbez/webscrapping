7f7410da1b25d38b3583b461d3fa64cb004a80a4
sadok-dorbez <sadokdorbez@gmail.com>
2023-04-06 23:43:02 +0100

    retrieve all articles

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-axios.get('https://www.somatra-get.com.tn/projets-en-cours.php') 
+axios.get('https://www.somatra-get.com.tn/projets-en-cours.php')
   .then((response) => {
     const html = response.data;
     const $ = cheerio.load(html);
 
-    const projectTitle = $('h3.post-title a').text();
-    const projectUrl = $('h3.post-title a').attr('href');
+    $('article').each((i, article) => {
+      const title = $(article).find('h2 a').text().trim();
+      const link = $(article).find('h2 a').attr('href');
+      const description = $(article).find('.entry-content p').text().trim();
 
-    console.log(projectTitle, projectUrl);
+      console.log(`Title: ${title}`);
+      console.log(`Link: ${link}`);
+      console.log(`Description: ${description}`);
+      console.log('----------------------');
+    });
   })
   .catch((error) => {
     console.log(error);
